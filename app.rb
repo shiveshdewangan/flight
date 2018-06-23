1579fa8ea7e3a7de70b87988c5d9b2008bb5d30c
Shivesh Dewangan <shiveshdewangan@Shiveshs-MacBook-Pro.local>
2018-06-23 11:54:21 +1000

    final version

--- app.rb
+++ app.rb
@@ -7,52 +7,84 @@ configure do
 	set :session_secret, "secret"
 end
 
+get "/flight/find" do
+	erb :search
+end
+
+def is_valid_airline?(airline_value)
+	airline_value.chars.each do |char|
+		if !("a".."z").to_a.include?(char)
+			return false
+		end
+	end
+end
+
+def is_valid_flight_number?(flight_number_value)
+	flight_number_value.chars.each do |char|
+		if !("0".."9").to_a.include?(char)
+			return false
+		end
+	end
+end
+
+def is_valid_destination?(destination_value)
+	destination_value.chars.each do |char|
+		if !("a".."z").to_a.include?(char)
+			return false
+		end
+	end
+end
+
+def is_valid_departure_time?(departure_time)
+  
+  return false if !departure_time.include?(":")
+  
+  hours, mins = departure_time.split(":")
+  
+  if (0..23).cover?(hours.to_i)
+    return true
+  elsif (0..59).cover?(mins.to_i)
+    return true
+  else
+    return false
+  end
+
+end
+
 get "/search" do
 
-	# unless validate_airline(params[:airline]) 
-	# if !validate_airline(params[:airline])
-	# 	redirect "/"
-	# end
+	session[:details] = {
+		"airline" => params[:airline],
+		"flight_number" => params[:flight_number],
+		"destination" => params[:destination],
+		"departure_time" => params[:departure_time]
+	}
 
-	session[:airline] = params[:airline]
-	session[:flight_number] = params[:flight_number]
-	session[:destination] = params[:destination]
-	session[:departure_time] = params[:departure_time]
+	@airline = session[:details]["airline"]
+	@flight_number = session[:details]["flight_number"]
+	@destination = session[:details]["destination"]
+	@departure_time = session[:details]["departure_time"]
 
-	# if [session[:airline], session[:flight_number], 
-	# 	session[:destination], session[:departure_time]].all? {|el| el == ""}
-	# 	session[:message] = "You have not entered any of the values."
-	# end
-
-	if (session[:airline] && session[:flight_number] && 
-		session[:destination] && session[:departure_time]) == ""
-		session[:message] = "You have not entered any of the values."
-		redirect "/search"
-	end
-
-	if session[:airline] == ""
-		session[:message] = "Airline field can not blank."
-		erb :search
-	elsif session[:flight_number] == ""
-		session[:message] = "Flight Number field can not be blank."
-		erb :search
-	elsif session[:destination] == ""
-		session[:message] = "Destination can not be blank."
-		erb :search
-	elsif session[:departure_time] == ""
-		session[:message] = "Departure Time can not be blank."
-		erb :search
+	if !is_valid_airline?(@airline)
+		session[:error] = "Invalid Airline entered. Please try again." 
+	elsif !is_valid_flight_number?(@flight_number)
+		session[:error] = "Invalid Flight Number entered. Please try again."
+	elsif !is_valid_destination?(@destination)
+		session[:error] = "Invalid Destination entered. Please try again."
+	elsif !is_valid_departure_time?(@departure_time)
+		session[:error] = "You have entered either invalid time or time in an invalid format. Please try again.
+						  Time must be in vaid format. E.g - 03:30"		
 	else
-		# session[:success] = "Your details are as below"
-		# redirect "/details"
+		session[:success] = "Thank you!!"
+		redirect "/details"
 	end
 
-
-
-	# if ![String].include?(session[:airline])
-	# 	session[:error] = "You have entered an invalid airline - #{session[:airline]}. 
-	# 					   Please enter the correct value"
-	# end
+	if ((!is_valid_airline?(@airline)) && 
+		(!is_valid_flight_number?(@flight_number)) && 
+		(!is_valid_destination?(@destination)) && 
+		(!is_valid_departure_time?(@departure_time)))
+			session[:error] = "You have entered invalid values. Please enter correct values."
+	end
 
 	erb :search
 end
@@ -62,43 +94,10 @@ get "/details" do
 end
 
 get "/" do
+	session[:details]["airline"] = ""
+	session[:details]["flight_number"] = ""
+	session[:details]["destination"] = ""
+	session[:details]["departure_time"]  = ""
+
 	erb :home
 end
-
-# get "/payments/create" do
-# 	erb :search
-# end
-
-# get "/search" do
-
-# 	session[:details] = {
-# 		"firstname" => params[:firstname],
-# 		"lastname" => params[:lastname],
-# 		"cardno" => params[:cardno],
-# 		"expirydate" => params[:expirydate],
-# 		"ccv" => params[:ccv]
-# 	}
-
-# 	session[:missing_values] = session[:details].keys.select do |k|
-# 		session[:details][k] == ""
-# 	end.join(", ")
-
-# 	if session[:details].values.include?("")
-# 		session[:error] = "#{session[:missing_values]} are missing"
-# 		erb :search
-# 	elsif (session[:details]["cardno"].size < 16)
-# 		session[:error] = "Card No must be greater than 16 digits"
-# 		erb :search
-# 	else
-# 		session[:details]["cardno"] = session[:details]["cardno"][0..11].split("")
-# 										.map{|c|c="*"}.each_slice(4).to_a
-# 										.map{|arr| arr.join}.join("-") + "-" + session[:details]["cardno"][-4..-1]
-# 		session[:details]["created_time"] = Time.new
-# 		session[:success] = "Thank you for your payment."
-
-# 		session["payment_count"] = 0
-
-# 		redirect "/payments"
-# 	end
-
-# end
